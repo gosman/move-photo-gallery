f577f7d25a0df616ce06f8dd54bcd07bf57e81f6
gosman <graham@dgcomms.co.uk>
2022-07-03 20:45:28 +0100

    Initial Commit

--- resources/views/shopify/client-gallery/index.blade.php
+++ resources/views/shopify/client-gallery/index.blade.php
@@ -115,53 +115,7 @@
 </div>
 
 
-<!-- Root element of PhotoSwipe. Must have class pswp. -->
-<div class="pswp" tabindex="-1" role="dialog" aria-hidden="true">
-    <!-- Background of PhotoSwipe.
-             It's a separate element as animating opacity is faster than rgba(). -->
-    <div class="pswp__bg"></div>
-    <!-- Slides wrapper with overflow:hidden. -->
-    <div class="pswp__scroll-wrap">
-        <!-- Container that holds slides.
-                  PhotoSwipe keeps only 3 of them in the DOM to save memory.
-                  Don't modify these 3 pswp__item elements, data is added later on. -->
-        <div class="pswp__container">
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-            <div class="pswp__item"></div>
-        </div>
-        <!-- Default (PhotoSwipeUI_Default) interface on top of sliding area. Can be changed. -->
-        <div class="pswp__ui pswp__ui--hidden">
-            <div class="pswp__top-bar">
-                <!--  Controls are self-explanatory. Order can be changed. -->
-                <div class="pswp__counter"></div>
-                <button class="pswp__button pswp__button--close" title="Close (Esc)"></button>
-                <button class="pswp__button pswp__button--share" title="Share"></button>
-                <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button>
-                <button class="pswp__button pswp__button--zoom" title="Zoom in/out"></button>
-                <!-- Preloader demo https://codepen.io/dimsemenov/pen/yyBWoR -->
-                <!-- element will get class pswp__preloader--active when preloader is running -->
-                <div class="pswp__preloader">
-                    <div class="pswp__preloader__icn">
-                        <div class="pswp__preloader__cut">
-                            <div class="pswp__preloader__donut"></div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
-                <div class="pswp__share-tooltip"></div>
-            </div>
-            <button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)">
-            </button>
-            <button class="pswp__button pswp__button--arrow--right" title="Next (arrow right)">
-            </button>
-            <div class="pswp__caption">
-                <div class="pswp__caption__center text-center"></div>
-            </div>
-        </div>
-    </div>
-</div>
+@include('shopify.client-gallery.photoswipe')
 
 
 <script>
@@ -625,7 +579,7 @@
         );
     };
 
-
+    //initialise photoswipe
     function initialisePhotoSwipe() {
 
         // Init empty gallery array
@@ -643,17 +597,11 @@
             galleryArray.push(item);
         });
 
-        console.log(galleryArray);
-
         // Define click event on gallery item
         $(document).on('click', '.g-image', function (event) {
 
-            console.log("here");
-
-            // Prevent location change
             event.preventDefault();
 
-            // Define object and gallery options
             var $pswp = $('.pswp')[0],
                 options = {
                     index: $(this).parent('figure').index(),
@@ -661,7 +609,6 @@
                     showHideOpacity: true
                 };
 
-            // Initialize PhotoSwipe
             var gallery = new PhotoSwipe($pswp, PhotoSwipeUI_Default, galleryArray, options);
             gallery.listen('gettingData', function (index, item) {
                 if ( item.w < 1 || item.h < 1 ) { // unknown size
@@ -676,6 +623,15 @@
                 }
             });
             gallery.init();
+
+            gallery.listen('init', () => {
+                $('#yotpo_testimonials_btn').css('visibility', 'hidden');
+            });
+
+            gallery.listen('close', () => {
+                $('#yotpo_testimonials_btn').css('visibility', 'visible');
+            });
+
         });
     }
 
