a4f3adb07663422233a9b72de0ffe5eaf5c8e93a
gosman <graham@dgcomms.co.uk>
2022-07-05 16:23:45 +0100

    Initial Commit

--- resources/views/shopify/client-gallery/index.blade.php
+++ resources/views/shopify/client-gallery/index.blade.php
@@ -75,27 +75,35 @@
                 </div>
             @endif
 
-            <div id="gallery" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                @foreach($images as $image)
-                    <div class="col">
-                        <div class="card shadow-sm g-image">
-                            <div class="content-overlay"></div>
-                            <a href="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}" title="{{$image->details->make}} {{$image->details->model}} | {{$image->details->year}} | {{$image->details->bumper_type}} {{$image->details->bumper_position}} Bumper" data-width="" data-height="100"></a>
-                            <img src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}">
+            @if($images-count())
+                <div id="gallery" class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+                    @foreach($images as $image)
+                        <div class="col">
+                            <div class="card shadow-sm g-image">
+                                <div class="content-overlay"></div>
+                                <a href="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}" title="{{$image->details->make}} {{$image->details->model}} | {{$image->details->year}} | {{$image->details->bumper_type}} {{$image->details->bumper_position}} Bumper" data-width="" data-height="100"></a>
+                                <img src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}">
 
-                            <div class="content-details fadeIn-bottom">
-                                <h3 class="content-title px-1">{{$image->details->make}} {{$image->details->model}}</h3>
-                                <p class="content-text px-1">{{$image->details->year}} | {{$image->details->bumper_type}} {{$image->details->bumper_position}} Bumper</p>
-                                <h3 class="content-title">
-                                    <svg height="50px" width="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
-                                        <path fill="white" d=" M312 196v24c0 6.6-5.4 12-12 12h-68v68c0 6.6-5.4 12-12 12h-24c-6.6 0-12-5.4-12-12v-68h-68c-6.6 0-12-5.4-12-12v-24c0-6.6 5.4-12 12-12h68v-68c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v68h68c6.6 0 12 5.4 12 12zm196.5 289.9l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L347.5 387.1c-2.3-2.3-3.5-5.3-3.5-8.5v-13.2c-36.5 31.5-84 50.6-136 50.6C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208c0 52-19.1 99.5-50.6 136h13.2c3.2 0 6.2 1.3 8.5 3.5l121.4 121.4c4.7 4.7 4.7 12.3 0 17zM368 208c0-88.4-71.6-160-160-160S48 119.6 48 208s71.6 160 160 160 160-71.6 160-160z"/>
-                                    </svg>
-                                </h3>
+                                <div class="content-details fadeIn-bottom">
+                                    <h3 class="content-title px-1">{{$image->details->make}} {{$image->details->model}}</h3>
+                                    <p class="content-text px-1">{{$image->details->year}} | {{$image->details->bumper_type}} {{$image->details->bumper_position}} Bumper</p>
+                                    <h3 class="content-title">
+                                        <svg height="50px" width="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
+                                            <path fill="white" d=" M312 196v24c0 6.6-5.4 12-12 12h-68v68c0 6.6-5.4 12-12 12h-24c-6.6 0-12-5.4-12-12v-68h-68c-6.6 0-12-5.4-12-12v-24c0-6.6 5.4-12 12-12h68v-68c0-6.6 5.4-12 12-12h24c6.6 0 12 5.4 12 12v68h68c6.6 0 12 5.4 12 12zm196.5 289.9l-22.6 22.6c-4.7 4.7-12.3 4.7-17 0L347.5 387.1c-2.3-2.3-3.5-5.3-3.5-8.5v-13.2c-36.5 31.5-84 50.6-136 50.6C93.1 416 0 322.9 0 208S93.1 0 208 0s208 93.1 208 208c0 52-19.1 99.5-50.6 136h13.2c3.2 0 6.2 1.3 8.5 3.5l121.4 121.4c4.7 4.7 4.7 12.3 0 17zM368 208c0-88.4-71.6-160-160-160S48 119.6 48 208s71.6 160 160 160 160-71.6 160-160z"/>
+                                        </svg>
+                                    </h3>
+                                </div>
                             </div>
                         </div>
+                    @endforeach
+                </div>
+            @else
+                <div class="container d-flex align-items-center justify-content-center pt-2">
+                    <div class="col-sm-12">
+                        Work in progress
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endif
         </div>
 
         <div class="container d-flex align-items-center justify-content-center pt-2">
