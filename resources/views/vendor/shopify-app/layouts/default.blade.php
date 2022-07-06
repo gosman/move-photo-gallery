fda47bc3f5d7986b80561ecfc00ed46d132d7f18
gosman <graham@dgcomms.co.uk>
2022-07-06 12:17:06 +0100

    Initial Commit

--- resources/views/vendor/shopify-app/layouts/default.blade.php
+++ resources/views/vendor/shopify-app/layouts/default.blade.php
@@ -5,6 +5,7 @@
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
         <title>{{ \Osiset\ShopifyApp\Util::getShopifyConfig('app_name') }}</title>
+        @vite('resources/css/app.css')
         @yield('styles')
     </head>
 
@@ -21,9 +22,9 @@
             <script src="https://unpkg.com/@shopify/app-bridge{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
             <script src="https://unpkg.com/@shopify/app-bridge-utils{{ \Osiset\ShopifyApp\Util::getShopifyConfig('appbridge_version') ? '@'.config('shopify-app.appbridge_version') : '' }}"></script>
             <script
-                @if(\Osiset\ShopifyApp\Util::getShopifyConfig('turbo_enabled'))
+                    @if(\Osiset\ShopifyApp\Util::getShopifyConfig('turbo_enabled'))
                     data-turbolinks-eval="false"
-                @endif
+                    @endif
             >
                 var AppBridge = window['app-bridge'];
                 var actions = AppBridge.actions;
