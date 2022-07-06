732ab59571b799c6e4fed49c92284e4a0f29c4a8
gosman <graham@dgcomms.co.uk>
2022-07-06 14:32:52 +0100

    Initial Commit

--- resources/views/shopify/index.blade.php
+++ resources/views/shopify/index.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <div class="mt-2 px-4 sm:px-6 lg:px-8">
-        <div class="grid grid-cols-2">
+        <div class="grid grid-cols-3">
             <div class="col-span-2">
                 <h1 class="text-xl font-semibold text-move-500">Submissions</h1>
             </div>
