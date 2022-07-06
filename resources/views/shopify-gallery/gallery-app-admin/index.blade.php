067947c7ed8464115a78afde3d9f04bc4769d7c6
gosman <graham@dgcomms.co.uk>
2022-07-06 16:14:11 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/index.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/index.blade.php
@@ -36,7 +36,7 @@
                                     <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
                                         <div class="flex items-center">
                                             <div class="h-10 w-10 flex-shrink-0">
-                                                <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="">
+                                                <img class="h-10 w-10 rounded-full" src="" alt="{{$submission->images[0]}}">
                                             </div>
                                             <div class="ml-4">
                                                 <div class="font-medium text-gray-900">Lindsay Walton</div>
