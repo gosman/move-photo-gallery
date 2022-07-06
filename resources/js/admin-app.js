ab7c702db182d3eb9c1ef5151aa484710f5baa71
gosman <graham@dgcomms.co.uk>
2022-07-06 17:49:33 +0100

    Initial Commit

--- resources/js/admin-app.js
+++ resources/js/admin-app.js
@@ -5,16 +5,16 @@ $(document).ready(function () {
 
     const redirect = actions.Redirect.create(app);
 
-    $('.shopify-link').on('click', function (e) {
-        e.preventDefault();
-        let url = $(this).attr('href');
-        redirect.dispatch(actions.Redirect.Action.APP, url);
-    })
-
     $('.app-link').on('click', function (e) {
         e.preventDefault();
         let url = $(this).attr('href');
         console.log(url);
+        redirect.dispatch(actions.Redirect.Action.APP, url);
+    })
+
+    $('.shopify-link').on('click', function (e) {
+        e.preventDefault();
+        let url = $(this).attr('href');
         redirect.dispatch(actions.Redirect.Action.ADMIN_PATH, url);
     })
 
