98ee502459563f50dc9b99042f05b00a400ac7dd
gosman <graham@dgcomms.co.uk>
2022-07-06 15:08:45 +0100

    Initial Commit

--- app/Http/Controllers/GalleryManagementController.php
+++ app/Http/Controllers/GalleryManagementController.php
@@ -10,7 +10,7 @@ class GalleryManagementController extends Controller
     public function index()
     {
 
-        $submissions = Submission::join('submission_images', 'submission.id', 'submission_images.submission_id')
+        $submissions = Submission::join('submission_images', 'id', 'submission_images.submission_id')
             ->orderBy('submission_images.approved')
             ->orderBy('submission_images.created_at')
             ->get();
