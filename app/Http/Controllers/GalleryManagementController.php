3b8435b46baac0e5b6aa46bbe28fd53ac9c4cf26
gosman <graham@dgcomms.co.uk>
2022-07-06 14:56:18 +0100

    Initial Commit

--- app/Http/Controllers/GalleryManagementController.php
+++ app/Http/Controllers/GalleryManagementController.php
@@ -2,11 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Submission;
+
 class GalleryManagementController extends Controller
 {
 
     public function index()
     {
+
+        return $submissions = Submission::with('images')
+            ->join('submission_images', 'submission_images.id', 'submission_id')
+            ->orderBy('submission_images.approved')
+            ->orderBy('submission_images.created_at')
+            ->get();
 
         return view('shopify-gallery.gallery-app-admin.index');
     }
