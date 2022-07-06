7af6c41894d0be1983b8069f23b16909d4e69bda
gosman <graham@dgcomms.co.uk>
2022-07-06 14:32:06 +0100

    Initial Commit

--- resources/views/shopify/index.blade.php
+++ resources/views/shopify/index.blade.php
@@ -3,11 +3,11 @@
 @section('content')
 
     <div class="mt-2 px-4 sm:px-6 lg:px-8">
-        <div class="columns-2">
-            <div class="w-3/4">
+        <div class="grid grid-cols-2">
+            <div class="col-span-2">
                 <h1 class="text-xl font-semibold text-move-500">Submissions</h1>
             </div>
-            <div class="w-1/4">
+            <div class="col-span-1">
                 <div class="mt-1">
                     <input type="text" name="name" id="name" class="shadow-sm focus:ring-move-500 focus:border-move-500 block w-full sm:text-sm border-gray-300 px-4 rounded-full" placeholder="Search">
                 </div>
