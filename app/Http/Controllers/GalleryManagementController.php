b936c3bc8faa80418911adf301e8a3b6e2ae21b5
gosman <graham@dgcomms.co.uk>
2022-07-06 16:08:23 +0100

    Initial Commit

--- app/Http/Controllers/GalleryManagementController.php
+++ app/Http/Controllers/GalleryManagementController.php
@@ -10,12 +10,15 @@ class GalleryManagementController extends Controller
     public function index()
     {
 
-        $submissionsApproved = Submission::with('images')->orderBy('created_at')->whereHas('images', function ($query) {
+        $submissionsUnapproved = Submission::with('images')
+            ->orderBy('created_at')
+            ->whereHas('images', function ($query) {
 
-            $query->where('approved', 1);
-        })->orderByDesc('created_at')->get();
+                $query->where('approved', 0);
+            })
+            ->get();
 
-        print_r($submissionsApproved->toArray());
+        print_r($submissionsUnapproved->toArray());
         exit;
 
         return view('shopify-gallery.gallery-app-admin.index');
