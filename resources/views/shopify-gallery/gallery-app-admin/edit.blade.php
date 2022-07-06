406799c2c2ebc12ac2fec9633f50e1d7ab93ada4
gosman <graham@dgcomms.co.uk>
2022-07-06 18:50:35 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -22,7 +22,7 @@
                             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="email" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Email address </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                    <input name="email" type="email" value="{{$submission->email}}" class="block max-w-lg w-full shadow-sm focus:ring-move-500 focus:border-move-500 sm:text-sm border-gray-300 rounded-md">
+                                    <input name="email" type="email" value="{{$submission->email}}" class="max-w-lg block w-full shadow-sm focus:ring-move-500 focus:border-move-500 sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
                                 </div>
                             </div>
 
