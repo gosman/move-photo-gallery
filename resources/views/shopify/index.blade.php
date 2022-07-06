02c9d3916b71e0db2a5711bf95393a1edad65a29
gosman <graham@dgcomms.co.uk>
2022-07-06 12:33:29 +0100

    Initial Commit

--- resources/views/shopify/index.blade.php
+++ resources/views/shopify/index.blade.php
@@ -9,7 +9,7 @@
                 <p class="mt-2 text-sm text-gray-700">A list of all the users in your account including their name, title, email and role.</p>
             </div>
             <div class="mt-4 sm:mt-0 sm:ml-16 sm:flex-none">
-                <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 sm:w-auto">Add user</button>
+                <button type="button" class="inline-flex items-center justify-center rounded-md border border-transparent bg-move-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-move-700 focus:outline-none focus:ring-2 focus:ring-move-500 focus:ring-offset-2 sm:w-auto">Add user</button>
             </div>
         </div>
         <div class="mt-8 flex flex-col">
@@ -50,7 +50,7 @@
                                 </td>
                                 <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
                                 <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                    <a href="#" class="text-move-600 hover:text-move-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                                 </td>
                             </tr>
 
