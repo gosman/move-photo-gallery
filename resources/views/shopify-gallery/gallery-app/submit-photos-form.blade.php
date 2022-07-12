da1e170bda1a98f226362af1b3894cbfbd32782b
gosman <graham@dgcomms.co.uk>
2022-07-12 14:28:30 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app/submit-photos-form.blade.php
+++ resources/views/shopify-gallery/gallery-app/submit-photos-form.blade.php
@@ -79,10 +79,15 @@
                     <span class="input-group-text">Bumper</span>
                     <select class="form-select required" id="uBumperType">
                         <option value="" disabled selected>Select bumper type</option>
-                        <option value="Standard">Standard</option>
-                        <option value="Prerunner">Prerunner</option>
-                        <option value="Full Grille">Full Grille</option>
-                        <option value="Embark">Embark</option>
+                        <option value="Series Classic">Series Classic</option>
+                        <option value="Series Precision">Series Precision</option>
+                        <option value="Series Embark">Series Embark</option>
+                        <option value="Series Bolt">Series Bolt</option>
+                        <option value="Series Moab">Series Moab</option>
+                        <option value="Series Trail">Series Trail</option>
+                        <option value="Series Overland">Series Overland</option>
+                        <option value="Series Heritage">Series Heritage</option>
+                        <option value="Series Switchback">Series Switchback</option>
                     </select>
                 </div>
             </div>
