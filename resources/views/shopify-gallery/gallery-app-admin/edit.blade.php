1c19f17e88dea125e49cf87ba9930ac5528293b2
gosman <graham@dgcomms.co.uk>
2022-07-12 20:38:52 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -96,7 +96,7 @@
                 </div>
 
                 <div class="pt-5">
-                    <div class="flex justify-end">
+                    <div class="flex justify-start">
                         <button type="button" class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-move-500">Cancel</button>
                         <button type="submit" class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-move-600 hover:bg-move-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-move-500">Save</button>
                     </div>
