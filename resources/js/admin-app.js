ac2ddfac68f366f99167ceceacc1927337c4a7f8
gosman <graham@dgcomms.co.uk>
2022-07-12 15:32:52 +0100

    Initial Commit

--- resources/js/admin-app.js
+++ resources/js/admin-app.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
         redirect.dispatch(actions.Redirect.Action.ADMIN_PATH, url);
     });
 
-    const makeModelYear = JSON.parse($("#makeModelYear").val());
+    const makeModelYear = $("#makeModelYear").val();
 
     console.log(makeModelYear);
 
