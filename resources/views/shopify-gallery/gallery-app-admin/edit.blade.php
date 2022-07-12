ad8b2669a8585734b381b42055ddf0e0efc619b9
gosman <graham@dgcomms.co.uk>
2022-07-12 21:20:56 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -8,16 +8,7 @@
             @foreach($submission->images as $image)
 
                 <img style="max-height: 150px!important" src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}"/>
-
-
-                <div class="mt-2">
-                    <div class="flex items-center h-5">
-                        <input id="comments" name="comments" type="checkbox" class="focus:ring-move-500 h-4 w-4 text-move-600 border-gray-300 rounded">
-                    </div>
-                    <div class="ml-1 text-xs">
-                        <label for="comments" class="text-gray-700">Approve</label>
-                    </div>
-                </div>
+                <input id="comments" name="comments" type="checkbox" class="focus:ring-move-500 h-4 w-4 text-move-600 border-gray-300 rounded">
 
             @endforeach
 
