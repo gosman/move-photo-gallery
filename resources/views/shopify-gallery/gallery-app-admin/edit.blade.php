296003288e9a0127d6206a35af12f84da6845133
gosman <graham@dgcomms.co.uk>
2022-07-12 21:12:06 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -11,7 +11,7 @@
                     <div class="flex items-center h-5">
                         <input id="comments" name="comments" type="checkbox" class="focus:ring-move-500 h-4 w-4 text-move-600 border-gray-300 rounded">
                     </div>
-                    <div class="ml-3 text-xs">
+                    <div class="ml-1 text-xs">
                         <label for="comments" class="text-gray-700">Approve</label>
                     </div>
                 </div>
@@ -20,7 +20,7 @@
                     <div class="flex items-center h-5">
                         <input id="comments" name="comments" type="checkbox" class="focus:ring-move-500 h-4 w-4 text-move-600 border-gray-300 rounded">
                     </div>
-                    <div class="ml-3 text-xs">
+                    <div class="ml-1 text-xs">
                         <label for="comments" class="text-gray-700">Approve</label>
                     </div>
                 </div>
