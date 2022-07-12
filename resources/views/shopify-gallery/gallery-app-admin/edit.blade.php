a41430edf94be9bec0a7eb2fe5e211e1c6f5d2ee
gosman <graham@dgcomms.co.uk>
2022-07-12 22:05:00 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -6,7 +6,7 @@
         <div class="columns-4 gap-2 mt-4">
 
             @foreach($submission->images as $image)
-                <img style="max-width: none; width:auto; max-height: 5rem;" src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}"/>
+                <img style="max-width: none; width:auto; max-height: 5em;" src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}"/>
                 <div class="relative flex items-start">
                     <div class="flex items-center h-5">
                         <input id="comments" name="comments" type="checkbox" class="focus:ring-indigo-500 h-4 w-4 text-indigo-600 border-gray-300 rounded">
