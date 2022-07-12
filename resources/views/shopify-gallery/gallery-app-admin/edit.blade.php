5a85e2b074096507b60e1c6cda2900c6e14645a9
gosman <graham@dgcomms.co.uk>
2022-07-12 15:46:44 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -29,10 +29,8 @@
                             <div class="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="country" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Country </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-2">
-                                    <select id="country" name="country" autocomplete="country-name" class="max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
-                                        <option>United States</option>
-                                        <option>Canada</option>
-                                        <option>Mexico</option>
+                                    <select id="truckMake" name="country" autocomplete="country-name" class="max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md">
+                                        <option disabled selected>SELECT MAKE</option>
                                     </select>
                                 </div>
                             </div>
