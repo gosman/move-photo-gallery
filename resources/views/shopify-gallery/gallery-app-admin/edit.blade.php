2d10e4c2958c3069d241e83718ac6fe73d97091a
gosman <graham@dgcomms.co.uk>
2022-07-12 21:23:46 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -7,7 +7,7 @@
 
             @foreach($submission->images as $image)
 
-                <img style="max-height: 150px!important, min-height: 50px!important" src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}"/>
+                <img style="max-height: 150px!important; min-height: 50px!important" src="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}"/>
                 <input id="comments" name="comments" type="checkbox" class="focus:ring-move-500 h-4 w-4 text-move-600 border-gray-300 rounded">
 
             @endforeach
