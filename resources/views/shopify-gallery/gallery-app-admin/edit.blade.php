d4cba3f6b8fc29c104f21cceddb9c19aa008e5a5
gosman <graham@dgcomms.co.uk>
2022-07-13 16:40:58 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -5,6 +5,13 @@
 
     <div class="px-4 sm:px-6 lg:px-8">
 
+        <button class="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
+            <svg class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z"/>
+            </svg>
+            <span>Previous</span>
+        </button>
+
         <div class="flex flex-col">
             <form class="space-y-8 divide-y divide-gray-200" action="/gallery-admin/submission/{{$submission->id}}" method="PUT">
                 <div class="space-y-8 divide-y divide-gray-200 sm:space-y-5">
