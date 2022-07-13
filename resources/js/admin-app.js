67d3ee7eab4292bbd264ed98bd22638514882a50
gosman <graham@dgcomms.co.uk>
2022-07-13 10:55:56 +0100

    Initial Commit

--- resources/js/admin-app.js
+++ resources/js/admin-app.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
             position: 'top',
             showConfirmButton: false,
             showCloseButton: true,
-            width: "80%",
+            height: "70%",
             didOpen: () => {
                 $(".swal2-popup").css("background-color", "transparent");
                 $(".swal2-close").css("background-color", "black");
