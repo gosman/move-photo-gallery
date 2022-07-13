7713a5b875fb78bd9f23d2a41b6c194e9e4929cf
gosman <graham@dgcomms.co.uk>
2022-07-13 08:42:20 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -26,17 +26,6 @@
                                 <div class="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
                                     <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
                                         <table class="min-w-full divide-y divide-gray-300">
-                                            <thead class="bg-gray-50">
-                                            <tr>
-                                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Name</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Title</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Status</th>
-                                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Role</th>
-                                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
-                                                    <span class="sr-only">Edit</span>
-                                                </th>
-                                            </tr>
-                                            </thead>
                                             <tbody class="divide-y divide-gray-200 bg-white">
                                             <tr>
                                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
@@ -50,14 +39,7 @@
                                                         </div>
                                                     </div>
                                                 </td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                    <div class="text-gray-900">Front-end Developer</div>
-                                                    <div class="text-gray-500">Optimization</div>
-                                                </td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
-                                                    <span class="inline-flex rounded-full bg-green-100 px-2 text-xs font-semibold leading-5 text-green-800">Active</span>
-                                                </td>
-                                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">Member</td>
+
                                                 <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
                                                     <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
                                                 </td>
