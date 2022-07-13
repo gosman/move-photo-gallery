3439fecbdf2fed6fa892bfd2bd3ca71748d996bc
gosman <graham@dgcomms.co.uk>
2022-07-13 15:46:23 +0100

    Initial Commit

--- resources/js/admin-app.js
+++ resources/js/admin-app.js
@@ -79,11 +79,6 @@ $(document).ready(function () {
 
         e.preventDefault();
 
-        $(this).html(`<svg class="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
-                                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                    </svg>`);
-
         let id = $(this).data('id');
         let imageUrl = $(this).data('image');
 
@@ -102,6 +97,11 @@ $(document).ready(function () {
             allowEnterKey: false,
             showLoaderOnConfirm: true,
             preConfirm: () => {
+
+                $(this).html(`<svg class="animate-spin -ml-1 mr-3 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+                                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+                                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+                                                    </svg>`);
 
                 return fetch(`/gallery-admin/image/${ id }`, {
                     'method': 'DELETE',
