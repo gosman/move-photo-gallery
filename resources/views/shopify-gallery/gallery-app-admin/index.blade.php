e4835e24ed952a094f01f4789ce0949b57eaf091
gosman <graham@dgcomms.co.uk>
2022-07-13 17:38:33 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/index.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/index.blade.php
@@ -31,7 +31,7 @@
             <div class="hidden sm:block">
                 <div class="border-b border-gray-200">
                     <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                        <a href="/" class="app-link border-indigo-500 text-move-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" aria-current="page">New Submissions</a>
+                        <a href="/" class="app-link border-move-500 text-move-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" aria-current="page">New Submissions</a>
                         <a href="/approved" class="app-link border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">Approved</a>
                     </nav>
                 </div>
