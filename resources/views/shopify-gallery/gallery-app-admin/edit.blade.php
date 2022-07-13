9c0929dfa0f937fd3525dc31c0485eefc1077b7a
gosman <graham@dgcomms.co.uk>
2022-07-13 17:15:44 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -105,13 +105,8 @@
                             <div class="sm:grid sm:grid-cols-4 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
                                 <label for="make" class="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"> Make/Model/Year </label>
                                 <div class="mt-1 sm:mt-0 sm:col-span-1">
-                                    <select data-selected="{{$submission->make}}" id="truckMake" name="make" class="max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"></select>
-                                    <select data-selected="{{$submission->model}}" id="truckModel" name="model" class="max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"></select>
-
-                                </div>
-                                <div class="mt-1 sm:mt-0 sm:col-span-1">
-                                </div>
-                                <div class="mt-1 sm:mt-0 sm:col-span-1">
+                                    <select data-selected="{{$submission->make}}" id="truckMake" name="make" class="mb-1 max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"></select>
+                                    <select data-selected="{{$submission->model}}" id="truckModel" name="model" class="mb-1 max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"></select>
                                     <select data-selected="{{$submission->year}}" id="truckYear" name="year" class="max-w-lg block focus:ring-move-500 focus:border-move-500 w-full shadow-sm sm:max-w-xs sm:text-sm border-gray-300 rounded-md"></select>
                                 </div>
                             </div>
