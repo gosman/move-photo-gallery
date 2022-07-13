2e9335704f15ba461b41f45932b098d9f282f284
gosman <graham@dgcomms.co.uk>
2022-07-13 17:36:43 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/index.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/index.blade.php
@@ -31,14 +31,8 @@
             <div class="hidden sm:block">
                 <div class="border-b border-gray-200">
                     <nav class="-mb-px flex space-x-8" aria-label="Tabs">
-                        <!-- Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" -->
-                        <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"> My Account </a>
-
-                        <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"> Company </a>
-
-                        <a href="#" class="border-indigo-500 text-indigo-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" aria-current="page"> Team Members </a>
-
-                        <a href="#" class="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm"> Billing </a>
+                        <a href="/" class="app-link border-indigo-500 text-move-600 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm" aria-current="page">New Submissions</a>
+                        <a href="/approved" class="app-link border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 whitespace-nowrap py-4 px-1 border-b-2 font-medium text-sm">Approved</a>
                     </nav>
                 </div>
             </div>
