82d039c2ab0eda388a738236102807d1ca5773e0
gosman <graham@dgcomms.co.uk>
2022-07-13 12:48:47 +0100

    Initial Commit

--- app/Http/Controllers/GalleryManagementController.php
+++ app/Http/Controllers/GalleryManagementController.php
@@ -47,7 +47,7 @@ class GalleryManagementController extends Controller
 
         if ($this->isAllowed()) {
 
-            SubmissionImage::find($imageId)->delete();
+            //SubmissionImage::find($imageId)->delete();
             sleep(100);
 
             return response()->json(['success' => true]);
