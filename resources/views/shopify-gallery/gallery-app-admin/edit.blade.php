969dcc0ddf730a7bbc4a54d3bb46a9d3ae122330
gosman <graham@dgcomms.co.uk>
2022-07-13 15:34:59 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -51,12 +51,8 @@
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
                                                         </svg>
-                                                        <span class="ml-3">Unapprove</span>
+                                                        <span class="ml-3">Unapproved</span>
                                                     </a>
-                                                    <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-red" xmlns="http://www.w3.org/2000/svg" fill="red" viewBox="0 0 24 24">
-                                                        <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
-                                                        <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
-                                                    </svg>
                                                 @else
                                                     <a href="#" data-id="{{$image->id}}" data-approve=1 class="image-approve relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-move-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
