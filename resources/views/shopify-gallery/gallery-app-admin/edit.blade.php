70fac5a2f3dc4d0be7419f9245e617bff1c65950
gosman <graham@dgcomms.co.uk>
2022-07-13 08:48:06 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -23,9 +23,9 @@
 
                         <div class="mt-8 flex flex-col">
                             <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                                <div class="inline-block sm:grid sm:grid-cols-3 py-2 align-middle md:px-6 lg:px-8">
+                                <div class="inline-block min-w-half py-2 align-middle md:px-6 lg:px-8">
                                     <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
-                                        <table class="sm:col-span-3 divide-y divide-gray-300">
+                                        <table class="min-w-half divide-y divide-gray-300">
                                             <tbody class="divide-y divide-gray-200 bg-white">
                                             <tr>
                                                 <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6">
