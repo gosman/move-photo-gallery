1cda0d8585c2284a098ec12d38f783e5fe18e2e8
gosman <graham@dgcomms.co.uk>
2022-07-13 10:01:01 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -26,7 +26,7 @@
                                     <div>
                                         <div class="-mt-px flex divide-x divide-gray-200">
                                             <div class="w-0 flex-1 flex">
-                                                <a href="#" class="relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
+                                                <a href="#" data-id="{{$image->id}}" class="image-delete relative -mr-px w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-bl-lg hover:text-gray-500">
                                                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-red-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                         <path stroke-linecap="round" stroke-linejoin="round" d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"/>
                                                     </svg>
@@ -34,12 +34,24 @@
                                                 </a>
                                             </div>
                                             <div class="-ml-px w-0 flex-1 flex">
-                                                <a href="#" class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-                                                    <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-move-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-                                                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"/>
-                                                    </svg>
-                                                    <span class="ml-3">Approve</span>
-                                                </a>
+
+                                                @if($image->approved)
+                                                    <a href="#" data-id="{{$image->id}}" class="image-unapprove relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-move-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"/>
+                                                        </svg>
+                                                        <span class="ml-3">Unapprove</span>
+                                                    </a>
+                                                @else
+                                                    <a href="#" data-id="{{$image->id}}" class="image-approve relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-move-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"/>
+                                                        </svg>
+                                                        <span class="ml-3">Approve</span>
+                                                    </a>
+                                                @endif
+
+
                                             </div>
                                         </div>
                                     </div>
