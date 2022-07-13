5eb687e4a5f1edaf5a77413ae2e1dfcf52d54174
gosman <graham@dgcomms.co.uk>
2022-07-13 11:22:22 +0100

    Initial Commit

--- public/js/admin-app.js
+++ public/js/admin-app.js
@@ -48,8 +48,7 @@ $(document).ready(function () {
       text: "Are you sure?",
       icon: 'warning',
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
+      confirmButtonColor: '#d33',
       confirmButtonText: 'Delete',
       preConfirm: function preConfirm() {}
     });
