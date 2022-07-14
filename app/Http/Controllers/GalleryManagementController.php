3b63d34975ff10e924b7f706b92cbca9fc3b1268
gosman <graham@dgcomms.co.uk>
2022-07-14 15:00:18 +0100

    Initial Commit

--- app/Http/Controllers/GalleryManagementController.php
+++ app/Http/Controllers/GalleryManagementController.php
@@ -40,7 +40,71 @@ class GalleryManagementController extends Controller
     public function search()
     {
 
-        print_r(request()->all());
+        $json = '[
+    {
+        "id": "Podiceps nigricollis",
+        "label": "Black-necked Grebe",
+        "value": "Black-necked Grebe"
+    },
+    {
+        "id": "Nycticorax nycticorax",
+        "label": "Black-crowned Night Heron",
+        "value": "Black-crowned Night Heron"
+    },
+    {
+        "id": "Tetrao tetrix",
+        "label": "Black Grouse",
+        "value": "Black Grouse"
+    },
+    {
+        "id": "Limosa limosa",
+        "label": "Black-tailed Godwit",
+        "value": "Black-tailed Godwit"
+    },
+    {
+        "id": "Chlidonias niger",
+        "label": "Black Tern",
+        "value": "Black Tern"
+    },
+    {
+        "id": "Larus marinus",
+        "label": "Great Black-backed Gull",
+        "value": "Great Black-backed Gull"
+    },
+    {
+        "id": "Larus fuscus",
+        "label": "Lesser Black-backed Gull",
+        "value": "Lesser Black-backed Gull"
+    },
+    {
+        "id": "Larus ridibundus",
+        "label": "Black-headed Gull",
+        "value": "Black-headed Gull"
+    },
+    {
+        "id": "Turdus merula",
+        "label": "Common Blackbird",
+        "value": "Common Blackbird"
+    },
+    {
+        "id": "Sylvia atricapilla",
+        "label": "Blackcap",
+        "value": "Blackcap"
+    },
+    {
+        "id": "Rissa tridactyla",
+        "label": "Black-legged Kittiwake",
+        "value": "Black-legged Kittiwake"
+    },
+    {
+        "id": "Aegypius monachus",
+        "label": "Eurasian Black Vulture",
+        "value": "Eurasian Black Vulture"
+    }
+]';
+
+
+        return response()->json(json_decode($json));
     }
 
 
