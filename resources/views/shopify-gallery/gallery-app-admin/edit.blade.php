bd1a8248085d7a3bd6d6a839fa910a061edcc948
gosman <graham@dgcomms.co.uk>
2022-07-14 09:21:14 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -39,7 +39,7 @@
                                             @else
                                                 <dd class="mt-3 text-center">
                                                     <span class="px-2 py-1 text-gray-600 text-xs font-medium bg-red-100 rounded-full">Unapproved</span>
-                                                    <a class="justify-center items-center" href="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}" download>
+                                                    <a class="flex justify-center items-center" href="{{config('filesystems.disks.images.cdn')}}{{$image->image_name}}" download>
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 justify-center items-center" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"/>
                                                         </svg>
