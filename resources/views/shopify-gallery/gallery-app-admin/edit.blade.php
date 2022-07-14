2c1589c0c7ed2d3b80f4e2c32c6ffd1f00b43a20
gosman <graham@dgcomms.co.uk>
2022-07-14 09:32:11 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
+++ resources/views/shopify-gallery/gallery-app-admin/edit.blade.php
@@ -43,6 +43,7 @@
                                                         <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 stroke-gray-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
                                                             <path stroke-linecap="round" stroke-linejoin="round" d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M9 19l3 3m0 0l3-3m-3 3V10"/>
                                                         </svg>
+                                                        <br>
                                                         <span class="inline-block text-gray-500">Download Original</span>
                                                     </a>
                                                 </dd>
