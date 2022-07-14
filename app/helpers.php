48b05960ede55b36f1636d0bc28106b5acce7417
gosman <graham@dgcomms.co.uk>
2022-07-14 12:22:42 +0100

    Initial Commit

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,19 @@
 <?php
 
 
+use Illuminate\Support\Str;
+
 function cdnPath($file)
 {
 
     return config('filesystems.disks.images.cdn').$file;
+}
+
+
+function originalImage($image)
+{
+
+    $originalImage = Str::replace('.jpg', '-original.jpg', $image);
+
+    return config('filesystems.disks.images.cdn').$originalImage;
 }
