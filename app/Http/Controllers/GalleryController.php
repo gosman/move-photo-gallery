803c07d8536a21cf263f5738834eeddb65b80e73
gosman <graham@dgcomms.co.uk>
2022-07-20 19:39:55 +0100

    Initial Commit

--- app/Http/Controllers/GalleryController.php
+++ app/Http/Controllers/GalleryController.php
@@ -50,7 +50,6 @@ class GalleryController extends Controller
     public function store(Request $request)
     {
 
-
         $submission = $request->except([
             'timestamp',
             'signature',
