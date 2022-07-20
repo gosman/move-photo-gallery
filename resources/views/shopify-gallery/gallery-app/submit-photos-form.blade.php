3902ac35b846294e1cfb00048477c3a189307c51
gosman <graham@dgcomms.co.uk>
2022-07-20 12:37:11 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app/submit-photos-form.blade.php
+++ resources/views/shopify-gallery/gallery-app/submit-photos-form.blade.php
@@ -62,35 +62,6 @@
                     </select>
                 </div>
             </div>
-
-            <div class="col-lg-4">
-                <div class="input-group has-validation mt-2">
-                    <span class="input-group-text">Bumper</span>
-                    <select class="form-select required" id="uBumperPosition">
-                        <option value="" disabled selected>Select bumper position</option>
-                        <option value="Front">Front</option>
-                        <option value="Rear">Rear</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="col-lg-4">
-                <div class="input-group has-validation mt-2">
-                    <span class="input-group-text">Bumper</span>
-                    <select class="form-select required" id="uBumperType">
-                        <option value="" disabled selected>Select bumper type</option>
-                        <option value="Series Classic">Series Classic</option>
-                        <option value="Series Precision">Series Precision</option>
-                        <option value="Series Embark">Series Embark</option>
-                        <option value="Series Bolt">Series Bolt</option>
-                        <option value="Series Moab">Series Moab</option>
-                        <option value="Series Trail">Series Trail</option>
-                        <option value="Series Overland">Series Overland</option>
-                        <option value="Series Heritage">Series Heritage</option>
-                        <option value="Series Switchback">Series Switchback</option>
-                    </select>
-                </div>
-            </div>
         </div>
 
         <form action="/target" class="fileuploader" id="dropzone">
