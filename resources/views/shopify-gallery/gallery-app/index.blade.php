08ee873632584a012ec816146ff4b56f77fd484e
gosman <graham@dgcomms.co.uk>
2022-07-20 18:05:08 +0100

    Initial Commit

--- resources/views/shopify-gallery/gallery-app/index.blade.php
+++ resources/views/shopify-gallery/gallery-app/index.blade.php
@@ -484,14 +484,15 @@
 
                     var html = `<div id="imagePreview${ imageIndex }" class="container d-flex align-items-center justify-content-center mt-3">
                                             <div class="preview">
-
+                                                    <div class="row">
                                                     <div class="col-12"><small>${ file.name }</small></div>
                                                     <div class="col-10">
                                                         <div id="progress1" class="progressbar"></div>
                                                     </div>
                                                     <div data-delete-id="${ imageIndex }" class="col-2 progress-delete"></div>
-
+                                                    </div>
                                             </div>
+
                                         </div>`;
 
                     $(".previews").append(html);
